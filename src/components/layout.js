c142c3cad291998e6440852606cd997375b6a74c
Hector del Angel <hectordelangel@gmail.com>
2019-12-05 15:19:42 -0800

    Basic dynamic navbar setup

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,8 +4,13 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import "./bootstrap.min.css";
 import "./layout.css";
+import Navbar from "./globals/Navbar";
 
-const Layout = ({ children }) => <>{children}</>;
+
+
+const Layout = ({ children }) => <>
+  <Navbar />
+  {children}</>;
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
