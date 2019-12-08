99dee8a7833e16e575df0746a6f28410e83d4ba3
Hector del Angel <hectordelangel@gmail.com>
2019-12-08 08:33:12 -0800

    Some quick styling to center title over BGImage

--- src/components/globals/BackgroundSection.js
+++ src/components/globals/BackgroundSection.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import BackgroundImage from  'gatsby-background-image';
 
-const BackgroundSection = ({
-  img,
-  styleClass,
-  title,
-  overlayColor,
-  children,
-}) => (
-  <div className="overlay">
-    <BackgroundImage className={styleClass} fluid={img}>
-      <h1 className="title text-white text-uppercase text-center display-4 font-weight-bold">
-        {title}
-      </h1>
-      {children}
-    </BackgroundImage>
+const BackgroundSection = ({ img, styleClass, title, children }) => (
+  <div>
+    <div className="image-container">
+      <BackgroundImage className={styleClass} fluid={img}>
+        {children}
+      </BackgroundImage>
+    </div>
+    <h1 className="head-title text-uppercase text-center display-4 font-weight-bold">
+      {title}
+    </h1>
   </div>
 )
 
 BackgroundSection.defaultProps = {
   title: "default title",
-  styleClass: "default-background"
+  styleClass: "default-background",
 }
 
 
