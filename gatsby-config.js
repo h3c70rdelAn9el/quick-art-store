aedb0c2d9cdc6c3f946ccec52e78ce1d8f0c2658
Hector del Angel <hectordelangel@gmail.com>
2019-12-11 14:20:31 -0800

    Add snipcart plugin, and apply API key to use

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Art Scratch`,
+    description: ``,
+    author: `@h3c70r`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -16,6 +16,13 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+    },
+    {
+      resolve: 'gatsby-plugin-snipcart',
+      options: {
+        apiKey: process.env.SNIPCART_API,
+        autopop: true
+      }
     },
     {
       resolve: `gatsby-source-contentful`,
