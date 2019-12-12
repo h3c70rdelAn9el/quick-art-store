68c142b04153f796293a04108dadcb94d9e60582
Hector del Angel <hectordelangel@gmail.com>
2019-12-11 16:06:22 -0800

    Change the fonts

--- src/components/globals/Footer.js
+++ src/components/globals/Footer.js
@@ -8,6 +8,7 @@ const Footer = () => {
           <div className="col-10 mr-auto col-md-6  text-capitalize text-muted">
             <p>all rights reserved &copy;{new Date().getFullYear().toString()}</p>
             <p className="text-muted">h3c70r</p>
+            <p className="text-muted">* use 4242424242424242</p>
           </div>
         </div>
       </div>
